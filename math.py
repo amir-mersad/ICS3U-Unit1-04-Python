1a82c723283c389d13d02e11bafacd5df1ada367
Ubuntu <ubuntu@ip-172-31-18-12.ec2.internal>
2019-09-13 18:46:11 +0000

    added program

---
+++ math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by Amir Mersad
+# Created on September 2019
+# This program does simple math
+
+
+def main():
+    print("If a rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(3*5))
+    print("Perimiter is {}cm".format(2*(3+5)))
+
+
+if __name__ == "__main__":
+    main()
